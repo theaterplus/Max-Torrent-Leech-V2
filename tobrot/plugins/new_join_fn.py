5355ba9c611998da4d5ef2cbd404519dcb5b82fa
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-11-25 11:12:20 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""⚡ JOIN TORRENT MIRROR  GROUP\n\n 👉Link: <a href="https://t.me/MaxxMirror">MAXX MIRROR GROUP</a>""", disable_web_page_preview=True)
+    await message.reply_text("""📮 Join our Movie & Series Channel\n<a href="https://t.me/itsmethepunisher">The PunisheR Exclusive</a>""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
